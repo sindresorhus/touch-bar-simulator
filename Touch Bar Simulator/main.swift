ded4c97e13ec16848a43f1170253f8e38323d963
Wayne Yeh <wayne.yeh@eten.com.tw>
2017-03-16 17:34:53 +0800

    add Transparency Slider

--- Touch Bar Simulator/main.swift
+++ Touch Bar Simulator/main.swift
@@ -14,6 +14,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
 	func applicationDidFinishLaunching(_ notification: Notification) {
 		controller.window?.delegate = self
 		addScreenshotButton()
+		addTransparencySlider()
 	}
 
 	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
@@ -41,6 +42,22 @@ final class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
 		keyDown?.flags = [.maskShift, .maskCommand]
 		keyDown?.post(tap: loc)
 		keyUp?.post(tap: loc)
+	}
+
+	func addTransparencySlider() {
+		let toolbarView = controller.window!.standardWindowButton(.closeButton)!.superview!
+		let slider = NSSlider()
+		slider.frame = CGRect(x: toolbarView.frame.width - 100, y: 4, width: 80, height: 11)
+		slider.action = #selector(transparency(sender:))
+		toolbarView.addSubview(slider)
+        
+		slider.minValue = 0.5
+		slider.doubleValue = 0.75
+		controller.window!.alphaValue = CGFloat(slider.doubleValue)
+	}
+
+	func transparency(sender : NSSlider) {
+		controller.window!.alphaValue = CGFloat(sender.doubleValue)
 	}
 }
 
