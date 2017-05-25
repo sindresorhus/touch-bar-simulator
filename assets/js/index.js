ecf4ec422ad03c8d82e88819267813d9e1353a9b
Sindre Sorhus <sindresorhus@gmail.com>
2017-05-25 23:37:40 +0700

    Tweaks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,3 @@
-$.get('https://api.github.com/repos/sindresorhus/touch-bar-simulator/releases/latest', function (data) {
-  $('#touchBar').attr('href', data.assets[0].browser_download_url);
+$.get('https://api.github.com/repos/sindresorhus/touch-bar-simulator/releases/latest', data => {
+	$('#download-button').attr('href', data.assets[0].browser_download_url);
 });
