44e36135ac50056864a3e17001c4883b384892f4
Sindre Sorhus <sindresorhus@gmail.com>
2024-03-06 23:53:08 +0700

    Meta tweaks

--- Touch Bar Simulator/App.swift
+++ Touch Bar Simulator/App.swift
@@ -14,7 +14,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 		$0.menu = with(NSMenu()) {
 			$0.delegate = self
 		}
-		$0.button!.image = NSImage(named: "MenuBarIcon")
+		$0.button!.image = .menuBarIcon
 		$0.button!.toolTip = "Right-click or option-click for menu"
 		$0.button!.preventsHighlight = true
 	}
